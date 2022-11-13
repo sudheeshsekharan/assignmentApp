38fdb00187cc48c997d731dddbfe63ce581d58f7
Sudheesh S <2021mt93619@wilp.bits-pilani.ac.in>
2022-11-13 15:19:46 +0530

    Added /user GET endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/:user', (req, res) => {
+    res.send('Hello '+req.params.user)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
